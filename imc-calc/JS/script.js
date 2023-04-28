3843ae11c1518ebe8e4e77ed7ba28a53e1a6849b
M4RC3LO75 <126880453+M4RC3LO75@users.noreply.github.com>
2023-04-28 11:57:32 -0300

    Update script.js

--- imc-calc/JS/script.js
+++ imc-calc/JS/script.js
@@ -15,30 +15,34 @@ btnCalc.addEventListener('click', (e) => {
     const weightValue = Number(weight.value);
     const imc = weightValue / (heightValue * heightValue);
     
-    rText.innerHTML = `Olá, ${nameValue}!<br/> Seu IMC é: ${imc.toFixed(2)} <br/>`
+    if (nameValue !='' && heightValue !='' && weightValue != '') {
+        rText.innerHTML = `Olá, ${nameValue}!<br/> Seu IMC é: ${imc.toFixed(2)} <br/>`
 
-    if (imc < 16) {
-        rText.innerHTML += "Magreza Grau 3<br/>Seu peso está abaixo do ideal."
-    }
-    if (imc >= 16 && imc <= 16.9) {
-        rText.innerHTML += "Magreza Grau 2<br/>Seu peso está abaixo do ideal."
-    }
-    if (imc >= 17 && imc <= 18.4) {
-        rText.innerHTML += "Magreza Grau 1<br/>Seu peso está abaixo do ideal."
-    }
-    if (imc >= 18.4 && imc <= 24.9) {
-        rText.innerHTML += "Adequado<br/>Seu peso está dentro do ideal."
-    }
-    if (imc >= 25 && imc <= 29.9) {
-        rText.innerHTML += "Pré-Obeso<br/>Seu peso está acima do ideal."
-    }
-    if (imc >= 30 && imc <= 34.9) {
-        rText.innerHTML += "Obesidade Grau 1<br/>Seu peso está acima do ideal."
-    }
-    if (imc >= 35 && imc <= 39.9) {
-        rText.innerHTML += "Obesidade Grau 2<br/>Seu peso está acima do ideal."
-    }
-    if (imc > 40) {
-        rText.innerHTML += "Obesidade Grau 3<br/>Seu peso está acima do ideal."
+        if (imc < 16) {
+            rText.innerHTML += "Magreza Grau 3<br/>Seu peso está abaixo do ideal."
+        }
+        if (imc >= 16 && imc <= 16.9) {
+            rText.innerHTML += "Magreza Grau 2<br/>Seu peso está abaixo do ideal."
+        }
+        if (imc >= 17 && imc <= 18.4) {
+            rText.innerHTML += "Magreza Grau 1<br/>Seu peso está abaixo do ideal."
+        }
+        if (imc >= 18.4 && imc <= 24.9) {
+            rText.innerHTML += "Adequado<br/>Seu peso está dentro do ideal."
+        }
+        if (imc >= 25 && imc <= 29.9) {
+            rText.innerHTML += "Pré-Obeso<br/>Seu peso está acima do ideal."
+        }
+        if (imc >= 30 && imc <= 34.9) {
+            rText.innerHTML += "Obesidade Grau 1<br/>Seu peso está acima do ideal."
+        }
+        if (imc >= 35 && imc <= 39.9) {
+            rText.innerHTML += "Obesidade Grau 2<br/>Seu peso está acima do ideal."
+        }
+        if (imc > 40) {
+            rText.innerHTML += "Obesidade Grau 3<br/>Seu peso está acima do ideal."
+        }
+    } else {
+        rText.innerHTML = 'Preencha todos os campos.'
     }
 })
