d58881c60eb9d71c4978d886522e8d37a6680342
M4RC3LO75 <126880453+M4RC3LO75@users.noreply.github.com>
2023-04-28 14:57:23 -0300

    JavaScript improved

--- lampada/JS/script.js
+++ lampada/JS/script.js
@@ -7,9 +7,11 @@ function breakbulb(){
         msg.style.background = 'red';
         msg.innerHTML = `A lâmpada está quebrada! </br>Clique aqui para restaurar.`
     } else {
+        var bodyBg = document.querySelector('#body');
         bulb.src = 'img/broken.jpg';
         bulb.classList.add('broken');
         btnSwitch.style.backgroundImage="url('./img/switch-off.png')";
+        bodyBg.style.background = 'linear-gradient(to top, black, #150886) fixed';
         msg.innerHTML = `A Lâmpada quebrou!</br>Tente ligá-la novamente`;
     }
 }
@@ -20,14 +22,17 @@ function switchbtn() {
         msg.innerHTML = `A lâmpada está quebrada! </br>Clique aqui para restaurar.`
         msg.style.cursor = 'pointer';
     } else {
+        var bodyBg = document.querySelector('#body');
         if (bulb.classList.contains('active')) {
             btnSwitch.style.backgroundImage="url('./img/switch-off.png')";
             bulb.src ='img/off.jpg';
             bulb.classList.remove('active');
+            bodyBg.style.background = 'linear-gradient(to top, black, #150886) fixed';
         } else {
             btnSwitch.style.backgroundImage="url('./img/switch-on.png')";
             bulb.src ='img/on.jpg';
             bulb.classList.add('active');
+            bodyBg.style.background = 'linear-gradient(to top, black, #dac82a) fixed';
         }
     }
 }
