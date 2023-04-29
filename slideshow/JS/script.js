b1a8c878f287166e9e70669464afaf6f3f6a2459
Marcelo Bastos <marcelobastos1975@gmail.com>
2023-04-29 16:44:34 -0300

    tiitle added for each image

--- slideshow/JS/script.js
+++ slideshow/JS/script.js
@@ -1,24 +1,26 @@
 'use strict';
 
 const images = [ 
-    {'id': '1', 'url':'./img/tomb-raider.png'},
-    {'id': '2', 'url':'./img/gta-v.jpg'},
-    {'id': '3', 'url':'./img/god-of-war-ragnarok.jpg'},
-    {'id': '4', 'url':'./img/far-cry-6.jpg'},
-    {'id': '5', 'url':'./img/donkey-kong-country.jpg'},
-    {'id': '6', 'url':'./img/resident-evil-6.jpg'},
-    {'id': '7', 'url':'./img/zelda-oot.jpg'},
-    {'id': '8', 'url':'./img/super-mario-64.jpg'},
+    {'id': '1', 'url':'./img/tomb-raider.png', 'title': `Tomb Raider`},
+    {'id': '2', 'url':'./img/gta-v.jpg', 'title': `GTA V`},
+    {'id': '3', 'url':'./img/god-of-war-ragnarok.jpg', 'title': `God of War - Ragnarok`},
+    {'id': '4', 'url':'./img/far-cry-6.jpg', 'title': `Far Cry 6`},
+    {'id': '5', 'url':'./img/donkey-kong-country.jpg', 'title': `Donkey Kong - Country`},
+    {'id': '6', 'url':'./img/resident-evil-6.jpg', 'title': `Resident Evil 6`},
+    {'id': '7', 'url':'./img/zelda-oot.jpg', 'title': `Zelda - Ocarina of Time`},
+    {'id': '8', 'url':'./img/super-mario-64.jpg', 'title': `Super Mario 64`},
 ]
 
 const containerItems = document.querySelector('#container-items');
+const titleItems = document.querySelector('#title')
 
 
 const loadImages = (images, container) => {
     images.forEach (image => {
         container.innerHTML += `
             <div class='item'>
-                <img src= '${image.url}'
+                <img src= '${image.url}'>
+                <p>${image.title}</p>
             </div>
         `
     })
