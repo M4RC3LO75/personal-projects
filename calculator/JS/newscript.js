6adc310845a8e38c0449d0b1b0d15d929fc02779
Marcelo Bastos <marcelobastos1975@gmail.com>
2023-05-03 20:59:48 -0300

    Script improved

--- calculator/JS/newscript.js
+++ calculator/JS/newscript.js
@@ -21,9 +21,19 @@ class Calculator {
 
     // Process all calculation operations
     processOperation(operation) {
+        // Check if current is empty
+        if (this.currentOperationText.innerText === "" && operation !== "C"){
+            // Change operation
+            if (this.previousOperationText.innerText !== '') {
+                this.changeOperation(operation);
+            }
+            return;
+        }
+
+
         // Get current and previous values
         let operationValue;
-        let previous = +this.previousOperationText.innerText;
+        let previous = +this.previousOperationText.innerText.split(' ')[0];
         let current = +this.currentOperationText.innerText;
 
         switch (operation) {
@@ -31,6 +41,33 @@ class Calculator {
                 operationValue = previous + current;
                 this.updateScreen(operationValue, operation, current, previous);
                 break;
+                case "-":
+                    operationValue = previous - current;
+                    this.updateScreen(operationValue, operation, current, previous);
+                    break;
+                case "*":
+                    operationValue = previous * current;
+                    this.updateScreen(operationValue, operation, current, previous);
+                    break;
+                case "/":
+                    operationValue = previous / current;
+                    this.updateScreen(operationValue, operation, current, previous);
+                    break;
+                case "«":
+                    this.processDelOperator();
+                    break;
+                case "CE":
+                    this.processClearCurrentOperator();
+                    break;
+                case "C":
+                    this.processClearOperator();
+                    break;
+                case "=":
+                    this.processEqualOperator();
+                    break;
+                case "±":
+                    this.processInvertOperator();
+                    break;
             default:
                 return;
         }
@@ -52,6 +89,44 @@ class Calculator {
             this.currentOperationText.innerText = '';
         }
     }
+
+    // Change Math operation
+    changeOperation(operation) {
+        const mathOperations = ['*', '/', '+', '-'];
+        if (!mathOperations.includes(operation)) {
+            return;
+        }
+
+        this.previousOperationText.innerText = this.previousOperationText.innerText.slice(0, -1) + operation;
+    }
+
+    // Delete a digit
+    processDelOperator() {
+        this.currentOperationText.innerText = this.currentOperationText.innerText.slice(0, -1);
+    }
+
+    // Clear current operation
+    processClearCurrentOperator() {
+        this.currentOperationText.innerText = '';
+    }
+
+    // Clear all operations
+    processClearOperator() {
+        this.currentOperationText.innerText = '';
+        this.previousOperationText.innerText = '';
+    }
+
+    // Process an operation
+    processEqualOperator() {
+        let operation = this.previousOperationText.innerText.split(' ')[1];
+
+        this.processOperation(operation);
+    }
+
+    // Invert to positive or negative number
+    processInvertOperator() {
+        this.currentOperationText.innerText = this.currentOperationText.innerText * -1;
+    }
 }
 
 const calc = new Calculator(previousOperationText, currentOperationText);
@@ -67,3 +142,35 @@ buttons.forEach((btn) => {
         }
     });
 });
+
+const keyboardMap = {
+    '0' : 'key0',
+    '1' : 'key1',
+    '2' : 'key2',
+    '3' : 'key3',
+    '4' : 'key4',
+    '5' : 'key5',
+    '6' : 'key6',
+    '7' : 'key7',
+    '8' : 'key8',
+    '9' : 'key9',
+    '+' : 'operationAdd',
+    '-' : 'operationSubtract',
+    '*' : 'operationMultiply',
+    '/' : 'operationDivide',
+    '=' : 'equals',
+    'Enter' : 'equals',
+    'c' : 'clearDisplay',
+    'Escape' : 'clearCalc',
+    'Backspace' : 'backspace',
+    ',' : 'decimal',
+
+}
+
+const mapKeyboard = (event) => {
+    const key = event.key;
+    const validKey = () => Object.keys(keyboardMap).indexOf(key) != -1;
+    if (validKey())
+    document.getElementById(keyboardMap[key]).click();
+}
+document.addEventListener('keydown', mapKeyboard)
